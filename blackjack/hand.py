5976a785a47137871037672b57dec9db42ce81df
vonhi-fra <franciszek.bern@yahoo.com>
2025-08-29 21:27:35 +0200

    deleted bj and casino first version

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -2,8 +2,8 @@ from blackjack.card import Card
 
 class Hand():
     __slots__ = ["cards", "bet"]
-    def __init__(self, bet = 0, cards = []):
-        self.cards = cards
+    def __init__(self, bet = 0, cards = None):
+        self.cards = cards if cards is not None else []
         self.bet = bet
     def __str__(self):
         value = str(self.bet) + "$"
@@ -37,7 +37,7 @@ class Hand():
     def Double(self):
         self.bet *= 2
     def split(self):
-        if len(self.cards) != 2 or self.cards[0] != self.cards[1]:
+        if len(self.cards) != 2 or self.cards[0].GetSymbol() != self.cards[1].GetSymbol():
             return ValueError("Can't split this!")
         hand_1 = Hand(self, self.bet, self.cards[0])
         hand_2 = Hand(self, self.bet, self.cards[1])
